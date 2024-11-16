1b1f463aba6e0f803a6403b8fd34e9fb92ad80c0
JibiGeorge <jibiyyan@gmail.com>
2024-11-16 16:03:34 +0530

    minor upsates on storage for redux

--- redux/store.ts
+++ redux/store.ts
@@ -1,20 +1,32 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import AsyncStorage from "@react-native-async-storage/async-storage"; // Use AsyncStorage for React Native
 import { usersSlice } from "./slice/userSlice";
-import storage from "redux-persist/lib/storage";
 
+// Configuring the persistence
 const persistConfig = {
   key: "root",
   version: 1,
-  storage,
+  storage: AsyncStorage, // Use AsyncStorage instead of the default storage
 };
 
+// Combine reducers
 export const reducer = combineReducers({
   users: usersSlice.reducer,
 });
 
+// Persist the reducer
 const persistedReducer = persistReducer(persistConfig, reducer);
 
+// Configure the store
 export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
