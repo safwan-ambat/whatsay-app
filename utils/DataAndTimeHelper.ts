34509c704ed560815c89bb62c262fd40bb66ebba
JibiGeorge <112756465+JibiGeorge@users.noreply.github.com>
2025-02-03 12:20:14 +0530

    getLast48HoursRange code updated for one month before format

--- utils/DataAndTimeHelper.ts
+++ utils/DataAndTimeHelper.ts
@@ -1,9 +1,18 @@
 export const getLast48HoursRange = () => {
-  const now = new Date();
-  const last24Hours = new Date(now.getTime() - 48 * 60 * 60 * 1000);
+  // const now = new Date();
+  // const last24Hours = new Date(now.getTime() - 48 * 60 * 60 * 1000);
 
-  return {
-    from: last24Hours.toISOString(),
-    to: now.toISOString(),
-  };
+  // return {
+  //   from: last24Hours.toISOString(),
+  //   to: now.toISOString(),
+  // };
+
+    const from = new Date();
+    const oneMonthBefore = new Date();
+    oneMonthBefore.setMonth(from.getMonth() - 1); // Subtract 1 month
+  
+    return {
+      from: oneMonthBefore.toISOString(),
+      to: from.toISOString(),
+    };
 };
